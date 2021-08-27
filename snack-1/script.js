fdc45d252be2dee86d2901604abab444a8b5ef2c
mariklim <maria.klimenko889@gmail.com>
2021-08-27 11:41:05 +0200

    soluzione con  for

---
+++ snack-1/script.js
@@ -0,0 +1,11 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+var listaNumeri = [];
+    for (var i = 0; i < 5; i++){
+    var numero = parseInt(prompt("Inserisci un numero"));
+    listaNumeri.push(numero);   
+} 
+console.log(numero*5);
+
