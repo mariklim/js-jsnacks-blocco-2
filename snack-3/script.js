09ce2107d11ea03a6c84fe2feaaf1619e48e0e16
mariklim <maria.klimenko889@gmail.com>
2021-08-27 17:06:43 +0200

    sistemato la creazione del nome completo

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,36 +1,48 @@
-// // Inserisci un numero, se è pari stampa il numero,
-// // se è dispari stampa il numero successivo
-
-
-// for (var i = 0; i <=4; i++){
-//     var numero = parseInt(prompt("Inserisci un numero"));
-//  if (numero % 2 == 0){
-//      console.log(numero);
-//  }
-//     else if (numero % 2 == 1){
-//         console.log(numero + 1);
-//  }
-
-// }
-
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
 // lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
-// var item = items[Math.floor(Math.random()*items.length)];
-var nomi = ["Mario", "Gigi", "Giuseppe", "Anna", "Lisa"]
-var cognomi = [" Rossi", "Verdi", "Gialli", "Azzurri", "Grassi"]
+//1.creo due archivi-array con i nomi e cognomi
+//2. creo un ciclo FOR per estrare random 3 nomi e 3 cognomi
+//3. creo un array vuoto per la lista di Invitati
+//4. unisco nome e cognome e lo metto nella lista
+//5. stampo la lista
 
-for (var i = 0; i<=3; i++){
+var nomi = ["Mario", "Gigi", "Giuseppe", "Anna", "Lisa"];
+var cognomi = [" Rossi", "Verdi", "Gialli", "Azzurri", "Grassi"];
+
+var nomeCompleto;
+var listaInvitati = [];
+for (var i = 0; i<3; i++ ){
+    // console.log("cognome nella lista sono: ");
+    // console.log(cognomi[i]);
+    // console.log("nome nella lista sono: ");
+    // console.log(nomi[i]);
     var nomeCasuale = nomi[Math.floor(Math.random()*nomi.length)];
-}
-
-for (var i = 0; i<=3; i++){
+    // console.log(nomeCasuale);
     var cognomeCasuale = cognomi[Math.floor(Math.random()*cognomi.length)];
+    // console.log(cognomeCasuale);
+    var nomeCompleto = nomeCasuale + " " + cognomeCasuale;
+    listaInvitati.push(nomeCompleto);
+    console.log(listaInvitati[i]);
+    alert("Ciao, " + listaInvitati[i] + ", sei nella lista!")
 }
-var nomeCompleto = nomeCasuale + cognomeCasuale;
 
-var listaInvitato = [];
 
-listaInvitato.push(nomeCompleto);
+// var item = items[Math.floor(Math.random()*items.length)];
+// var nomi = ["Mario", "Gigi", "Giuseppe", "Anna", "Lisa"]
+// var cognomi = [" Rossi", "Verdi", "Gialli", "Azzurri", "Grassi"]
 
-console.log(listaInvitato);
+// for (var i = 0; i<=3; i++){
+//     var nomeCasuale = nomi[Math.floor(Math.random()*nomi.length)];
+// }
+
+// for (var i = 0; i<=3; i++){
+//     var cognomeCasuale = cognomi[Math.floor(Math.random()*cognomi.length)];
+// }
+// var nomeCompleto = nomeCasuale + cognomeCasuale;
+
+// var listaInvitato = [];
+
+// listaInvitato.push(nomeCompleto);
+
+// console.log(listaInvitato);
